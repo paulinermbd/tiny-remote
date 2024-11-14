20869c58df528b04164d1d886b0cf027f03c9ac0
Pauline RAMBAUD <p.rambaud.pr@gmail.com>
2024-11-14 13:51:07 +0100

    feat(ble): test from keyboard ble

--- main.go
+++ main.go
@@ -14,13 +14,6 @@ func main() {
 	// Enable BLE interface.
 	must("enable BLE stack", adapter.Enable())
 
-	// Define the peripheral device info.
-	adv := adapter.DefaultAdvertisement()
-	must("config adv", adv.Configure(bluetooth.AdvertisementOptions{
-		LocalName:    "tinyremote",
-		ServiceUUIDs: []bluetooth.UUID{bluetooth.ServiceUUIDHumanInterfaceDevice},
-	}))
-
 	adapter.SetConnectHandler(func(device bluetooth.Device, connected bool) {
 		if connected {
 			println("connected, not advertising...")
@@ -31,7 +24,17 @@ func main() {
 			advUntil = time.Now().Add(5 * time.Minute)
 		}
 	})
-	must("start adv", adv.Start())
+
+	// Define the peripheral device info.
+	adv := adapter.DefaultAdvertisement()
+	must("config adv", adv.Configure(bluetooth.AdvertisementOptions{
+		LocalName: "tinyremote",
+		ServiceUUIDs: []bluetooth.UUID{
+			bluetooth.ServiceUUIDDeviceInformation,
+			bluetooth.ServiceUUIDBattery,
+			bluetooth.ServiceUUIDHumanInterfaceDevice,
+		},
+	}))
 
 	/*must("add service", adapter.AddService(&bluetooth.Service{
 		UUID: bluetooth.ServiceUUIDHumanInterfaceDevice,
@@ -45,7 +48,116 @@ func main() {
 		},
 	}))*/
 
+	adapter.AddService(&bluetooth.Service{
+		UUID: bluetooth.ServiceUUIDDeviceInformation,
+		Characteristics: []bluetooth.CharacteristicConfig{
+			{
+				UUID:  bluetooth.CharacteristicUUIDManufacturerNameString,
+				Flags: bluetooth.CharacteristicReadPermission,
+				Value: []byte("Nice Keyboards"),
+			},
+			{
+				UUID:  bluetooth.CharacteristicUUIDModelNumberString,
+				Flags: bluetooth.CharacteristicReadPermission,
+				Value: []byte("nice!nano"),
+			},
+			{
+				UUID:  bluetooth.CharacteristicUUIDPnPID,
+				Flags: bluetooth.CharacteristicReadPermission,
+				Value: []byte{0x02, 0x8a, 0x24, 0x66, 0x82, 0x34, 0x36},
+				//Value: []byte{0x02, uint8(0x10C4 >> 8), uint8(0x10C4 & 0xff), uint8(0x0001 >> 8), uint8(0x0001 & 0xff)},
+			},
+		},
+	})
+	adapter.AddService(&bluetooth.Service{
+		UUID: bluetooth.ServiceUUIDBattery,
+		Characteristics: []bluetooth.CharacteristicConfig{
+			{
+				UUID:  bluetooth.CharacteristicUUIDBatteryLevel,
+				Value: []byte{80},
+				Flags: bluetooth.CharacteristicReadPermission | bluetooth.CharacteristicNotifyPermission,
+			},
+		},
+	})
+
+	adapter.AddService(&bluetooth.Service{
+		UUID: bluetooth.ServiceUUIDGenericAccess,
+		Characteristics: []bluetooth.CharacteristicConfig{
+			{
+				UUID:  bluetooth.CharacteristicUUIDDeviceName,
+				Flags: bluetooth.CharacteristicReadPermission,
+				Value: []byte("tinygo-corne"),
+			},
+			{
+
+				UUID:  bluetooth.New16BitUUID(0x2A01),
+				Flags: bluetooth.CharacteristicReadPermission,
+				Value: []byte{uint8(0x03c4 >> 8), uint8(0x03c4 & 0xff)}, /// []byte(strconv.Itoa(961)),
+			},
+			// {
+			// 	UUID:  bluetooth.CharacteristicUUIDPeripheralPreferredConnectionParameters,
+			// 	Flags: bluetooth.CharacteristicReadPermission,
+			// 	Value: []byte{0x02},
+			// },
+
+			// // 		//
+		},
+	})
+
+	// hid
+	adapter.AddService(&bluetooth.Service{
+		UUID: bluetooth.ServiceUUIDHumanInterfaceDevice,
+		/*
+			 - hid information r
+			 - report map r
+			 - report nr
+			   - client charecteristic configuration
+			   - report reference
+			- report nr
+			   - client charecteristic configuration
+			   - report reference
+			- hid control point wnr
+		*/
+		Characteristics: []bluetooth.CharacteristicConfig{
+			// {
+			// 	UUID:  bluetooth.CharacteristicUUIDHIDInformation,
+			// 	Flags: bluetooth.CharacteristicReadPermission,
+			// 	Value: []byte{uint8(0x0111 >> 8), uint8(0x0111 & 0xff), uint8(0x0002 >> 8), uint8(0x0002 & 0xff)},
+			// },
+			{
+				//Handle: &reportmap,
+				UUID:  bluetooth.CharacteristicUUIDReportMap,
+				Flags: bluetooth.CharacteristicReadPermission,
+				Value: []byte("reportMap"),
+			},
+			{
+
+				//Handle: &reportIn,
+				UUID:  bluetooth.CharacteristicUUIDReport,
+				Value: []byte("test"),
+				Flags: bluetooth.CharacteristicReadPermission | bluetooth.CharacteristicNotifyPermission,
+			},
+			{
+				// protocl mode
+				UUID:  bluetooth.New16BitUUID(0x2A4E),
+				Flags: bluetooth.CharacteristicWriteWithoutResponsePermission | bluetooth.CharacteristicReadPermission,
+				// Value: []byte{uint8(1)},
+				// WriteEvent: func(client bluetooth.Connection, offset int, value []byte) {
+				// 	print("protocol mode")
+				// },
+			},
+			{
+				UUID:  bluetooth.CharacteristicUUIDHIDControlPoint,
+				Flags: bluetooth.CharacteristicWriteWithoutResponsePermission,
+				//	Value: []byte{0x02},
+			},
+		},
+	})
+
 	println("advertising...")
+
+	must("start adv", adv.Start())
+
 	address, _ := adapter.Address()
 	for {
 		if advState && time.Now().After(advUntil) {
